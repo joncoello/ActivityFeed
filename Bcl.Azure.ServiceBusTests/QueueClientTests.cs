51c65e97f89d9f5131e6fbaac7dd00795f738801
Jon Coello <joncoello@gmail.com>
2016-11-18 15:46:36 +0000

    we've got a proper queue

---
+++ Bcl.Azure.ServiceBusTests/QueueClientTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bcl.Azure.ServiceBus;
+
+namespace Bcl.Azure.ServiceBusTests
+{
+    [TestClass]
+    public class QueueClientTests
+    {
+        [TestMethod]
+        public void QueueClient_Create()
+        {
+            var client = new QueueClient();
+        }
+
+        [TestMethod]
+        public void QueueClient_Enqueue()
+        {
+            var client = new QueueClient();
+            client.Enqueue("blah");
+        }
+
+        [TestMethod]
+        public void QueueClient_Dequeue()
+        {
+            var client = new QueueClient();
+            var msg = client.Dequeue();
+        }
+
+        [TestMethod]
+        public void QueueClient_EnqueueAndDequeue()
+        {
+            var client = new QueueClient();
+
+            client.Enqueue("blah");
+
+            var msg = client.Dequeue();
+
+            Assert.AreEqual("blah", msg);
+
+        }
+
+        [TestMethod]
+        public void QueueClient_EnqueueMultiAndDequeueFirst()
+        {
+            var client = new QueueClient();
+
+            client.Enqueue("blah1");
+            client.Enqueue("blah2");
+            client.Enqueue("blah3");
+
+            var msg = client.Dequeue();
+
+            Assert.AreEqual("blah1", msg);
+        }
+
+    }
+
+}
