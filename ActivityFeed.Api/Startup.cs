e56caa0f142358448cefaffacadbb3a760133e30
Jon Coello <joncoello@gmail.com>
2016-11-22 09:02:02 +0000

    add hello world endpoint

--- ActivityFeed.Api/Startup.cs
+++ ActivityFeed.Api/Startup.cs
@@ -3,12 +3,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 
 namespace ActivityFeed.Api
 {
     public class Startup
     {
         public void Configuration(IAppBuilder app) {
+
+            var config = new HttpConfiguration();
+
+            // web api
+            config.MapHttpAttributeRoutes();
+            app.UseWebApi(config);
 
         }
     }
